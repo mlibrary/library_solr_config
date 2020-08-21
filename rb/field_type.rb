e5520301290bb8744c267ab189143ba4d941d464
Esty Thomas <esty@umich.edu>
2020-08-21 12:03:21 -0400

    Implement all the types from primitive_types.xml

--- rb/field_type.rb
+++ rb/field_type.rb
@@ -1,11 +1,383 @@
 class FieldType
     def self.get_type(name)
-        BinaryType.new
+        case name
+        when "binary"
+            BinaryType.new
+        when "boolean"
+            BooleanType.new
+        when "date"
+            DateType.new
+        when "double"
+            DoubleType.new
+        when "float"
+            FloatType.new
+        when "int"
+            IntType.new
+        when "long"
+            LongType.new
+        when 'string'
+            StringType.new
+        when 'facet'
+            FacetType.new
+        when 'single_date_stored'
+            SingleDateStoredType.new
+        when 'ignored'
+            IgnoredType.new
+        when 'location_rpt'
+            LocationRptType.new
+        when 'point'
+            PointType.new
+        end
     end
 end
 
 class BinaryType
     def solr_class
         "solr.BinaryField"
+    end
+
+    def name
+        "binary"
+    end
+
+    def to_json
+        {
+            "class" => solr_class,
+            "name" => name
+        }.to_json
+    end
+end
+
+class BooleanType
+    def solr_class
+        "solr.BoolField"
+    end
+
+    def name
+        "boolean"
+    end
+
+    def sort_missing_last
+        true
+    end
+
+    def to_json
+        {
+            "class": solr_class,
+            "name": name,
+            "sortMissingLast": sort_missing_last
+        }.to_json
+    end
+end
+
+class SolrTrieField
+    def precision_step
+        0
+    end
+
+    def position_increment_gap
+        0
+    end
+
+    def to_json
+        {
+            "class": solr_class,
+            "name": name,
+            "precisionStep": precision_step,
+            "positionIncrementGap": position_increment_gap
+        }.to_json
+    end
+end
+
+class DateType < SolrTrieField
+    def solr_class
+        "solr.TrieDateField"
+    end
+
+    def name
+        "date"
+    end
+
+    def precision_step
+        0
+    end
+
+    def position_increment_gap
+        0
+    end
+end
+
+class DoubleType < SolrTrieField
+    def name
+        "double"
+    end
+
+    def solr_class
+        "solr.TrieDoubleField"
+    end
+end
+
+class FloatType < SolrTrieField
+    def name
+        "float"
+    end
+
+    def solr_class
+        "solr.TrieFloatField"
+    end
+end
+
+class IntType < SolrTrieField
+    def name
+        "int"
+    end
+
+    def solr_class
+        "solr.TrieIntField"
+    end
+
+    def doc_values
+        true
+    end
+
+    def to_json
+        {
+            "class": solr_class,
+            "name": name,
+            "precisionStep": precision_step,
+            "positionIncrementGap": position_increment_gap,
+            "docValues": doc_values
+        }.to_json
+    end
+end
+
+class LongType < SolrTrieField
+    def name
+        "long"
+    end
+
+    def solr_class
+        "solr.TrieLongField"
+    end
+
+    def doc_values
+        true
+    end
+
+    def to_json
+        {
+            "class": solr_class,
+            "name": name,
+            "precisionStep": precision_step,
+            "positionIncrementGap": position_increment_gap,
+            "docValues": doc_values
+        }.to_json
+    end
+end
+
+class StringType
+    def name
+        "string"
+    end
+
+    def solr_class
+        "solr.StrField"
+    end
+
+    def stored
+        false
+    end
+
+    def indexed
+        false
+    end
+
+    def multi_valued
+        false
+    end
+
+    def doc_values
+        true
+    end
+
+    def to_json
+        {
+            "class": solr_class,
+            "name": name,
+            "stored": stored,
+            "indexed": indexed,
+            "multiValued": multi_valued,
+            "docValues": doc_values
+        }.to_json
+    end
+end
+
+class FacetType
+    def name
+        "facet"
+    end
+
+    def solr_class
+        "solr.StrField"
+    end
+
+    def stored
+        false
+    end
+
+    def indexed
+        true
+    end
+
+    def multi_valued
+        true
+    end
+
+    def doc_values
+        true
+    end
+
+    def to_json
+        {
+            "class": solr_class,
+            "name": name,
+            "stored": stored,
+            "indexed": indexed,
+            "multiValued": multi_valued,
+            "docValues": doc_values
+        }.to_json
+    end
+end
+
+class SingleDateStoredType
+    def name
+        "single_date_stored"
+    end
+
+    def solr_class
+        "solr.TrieDateField"
+    end
+
+    def stored
+        false
+    end
+
+    def indexed
+        false
+    end
+
+    def multi_valued
+        false
+    end
+
+    def doc_values
+        true
+    end
+
+    def to_json
+        {
+            "class": solr_class,
+            "name": name,
+            "stored": stored,
+            "indexed": indexed,
+            "multiValued": multi_valued,
+            "docValues": true
+        }.to_json
+    end
+end
+
+class IgnoredType
+    def solr_class
+        "solr.StrField"
+    end
+
+    def name
+        "ignored"
+    end
+
+    def stored
+        false
+    end
+
+    def indexed
+        false
+    end
+
+    def multi_valued
+        true
+    end
+
+    def to_json
+        {
+            "class": solr_class,
+            "name": name,
+            "stored": stored,
+            "indexed": indexed,
+            "multiValued": multi_valued
+        }.to_json
+    end
+end
+
+class LocationRptType
+    def name
+        "location_rpt"
+    end
+
+    def solr_class
+        "solr.SpatialRecursivePrefixTreeFieldType"
+    end
+
+    def geo
+        true
+    end
+
+    def dist_err_pct
+        0.025
+    end
+
+    def max_dist_err
+        0.000009
+    end
+
+    def distance_units
+        "degrees"
+    end
+
+    def to_json
+        {
+            "class": solr_class,
+            "name": name,
+            "geo": geo,
+            "distErrPct": dist_err_pct,
+            "maxDistErr": max_dist_err,
+            "distanceUnits": distance_units
+        }.to_json
+    end
+end
+
+class PointType
+    def solr_class
+        "solr.PointType"
+    end
+
+    def name
+        "point"
+    end
+
+    def dimension
+        2
+    end
+
+    def sub_field_suffix
+        "d"
+    end
+
+    def to_json
+        {
+            "class": solr_class,
+            "name": name,
+            "dimension": dimension,
+            "subFieldSuffix": sub_field_suffix
+        }.to_json
     end
 end
--- test/fieldtype_test.rb
+++ test/fieldtype_test.rb
@@ -3,6 +3,139 @@ require_relative 'test_helper'
 class FieldTypeTest < Minitest::Test
     def test_primitive_binary
         bin_type = FieldType.get_type('binary')
-        assert_equal bin_type.solr_class, "solr.BinaryField"
+        assert_equal bin_type.to_json, {
+            "class": "solr.BinaryField",
+            "name": "binary"
+        }.to_json
+    end
+
+    def test_primitive_bool
+        bin_type = FieldType.get_type('boolean')
+        assert_equal bin_type.to_json, {
+            "class": "solr.BoolField",
+            "name": "boolean",
+            "sortMissingLast": true
+        }.to_json
+    end
+
+    def test_primitive_date
+        bin_type = FieldType.get_type('date')
+        assert_equal bin_type.to_json, {
+            "class": "solr.TrieDateField",
+            "name": "date",
+            "precisionStep": 0,
+            "positionIncrementGap": 0
+        }.to_json
+    end
+
+    def test_primitive_double
+        bin_type = FieldType.get_type('double')
+        assert_equal bin_type.to_json, {
+            "class": "solr.TrieDoubleField",
+            "name": "double",
+            "precisionStep": 0,
+            "positionIncrementGap": 0
+        }.to_json
+    end
+
+    def test_primitive_float
+        bin_type = FieldType.get_type('float')
+        assert_equal bin_type.to_json, {
+            "class": "solr.TrieFloatField",
+            "name": "float",
+            "precisionStep": 0,
+            "positionIncrementGap": 0
+        }.to_json
+    end
+
+    def test_primitive_int
+        bin_type = FieldType.get_type('int')
+        assert_equal bin_type.to_json, {
+            "class": "solr.TrieIntField",
+            "name": "int",
+            "precisionStep": 0,
+            "positionIncrementGap": 0,
+            "docValues": true
+        }.to_json
+    end
+
+    def test_primitive_long
+        bin_type = FieldType.get_type('long')
+        assert_equal bin_type.to_json, {
+            "class": "solr.TrieLongField",
+            "name": "long",
+            "precisionStep": 0,
+            "positionIncrementGap": 0,
+            "docValues": true
+        }.to_json
+    end
+
+    def test_primitive_string
+        bin_type = FieldType.get_type('string')
+        assert_equal bin_type.to_json, {
+            "class": "solr.StrField",
+            "name": "string",
+            "stored": false,
+            "indexed": false,
+            "multiValued": false,
+            "docValues": true
+        }.to_json
+    end
+
+    def test_primitive_facet
+        bin_type = FieldType.get_type('facet')
+        assert_equal bin_type.to_json, {
+            "class": "solr.StrField",
+            "name": "facet",
+            "stored": false,
+            "indexed": true,
+            "multiValued": true,
+            "docValues": true
+        }.to_json
+    end
+
+    def test_primitive_single_date_stored
+        bin_type = FieldType.get_type('single_date_stored')
+        assert_equal bin_type.to_json, {
+            "class": "solr.TrieDateField",
+            "name": "single_date_stored",
+            "stored": false,
+            "indexed": false,
+            "multiValued": false,
+            "docValues": true
+        }.to_json
+    end
+
+    def test_primitive_ignored
+        bin_type = FieldType.get_type('ignored')
+        assert_equal bin_type.to_json, {
+            "class": "solr.StrField",
+            "name": "ignored",
+            "stored": false,
+            "indexed": false,
+            "multiValued": true
+        }.to_json
+    end
+
+    def test_primitive_location_rpt
+        bin_type = FieldType.get_type('location_rpt')
+        assert_equal bin_type.to_json, {
+            "class": "solr.SpatialRecursivePrefixTreeFieldType",
+            "name": "location_rpt",
+            "geo": true,
+            "distErrPct": 0.025,
+            "maxDistErr": 0.000009,
+            "distanceUnits": "degrees"
+        }.to_json
+    end
+
+    def test_primitive_point
+        bin_type = FieldType.get_type('point')
+        assert_equal bin_type.to_json, {
+            "class": "solr.PointType",
+            "name": "point",
+            "dimension": 2,
+            "subFieldSuffix": "d"
+        }.to_json
     end
 end
